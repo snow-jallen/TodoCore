d3099cc1f665ce544a422ad738ea4fc698ebaff1
Jonathan Allen <jallen@snow.edu>
2019-01-18 15:49:33 -0700

    Add Items DbSet, perform migration to add to database

--- TodoCore/Data/ApplicationDbContext.cs
+++ TodoCore/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TodoCore.Models;
 
 namespace TodoCore.Data
 {
@@ -12,5 +13,7 @@ namespace TodoCore.Data
             : base(options)
         {
         }
+
+        public DbSet<TodoItem> Items { get; set; }
     }
 }
